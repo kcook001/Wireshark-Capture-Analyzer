84131ded6f2ce006f88aff3ec0b676327b6000f8
kcook001 <kcook001@gmail.com>
2018-05-29 21:12:14 -0700

    Refactor, make more modular!

--- src/Tracker.java
+++ src/Tracker.java
@@ -1,37 +1,447 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Tracker {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		if(args.length != 2) {
-			System.out.println("Incorrect number of arguments.");
-			return;
+		//File names
+		String file1 = "EBtest1.csv";
+		String file2 = "EBtest2.csv";
+		String file3 = "EBtest3.csv";
+		String file4 = "LIBtest1.csv";
+		String file5 = "LIBtest2.csv";
+		String file6 = "LIBtest3.csv";
+		String file7 = "RECtest1.csv";
+		String file8 = "RECtest2.csv";
+		
+		//MAC addresses
+		List<String> l1 = load(file1);
+		List<String> l2 = load(file2);
+		List<String> l3 = load(file3);
+		List<String> l4 = load(file4);
+		List<String> l5 = load(file5);
+		List<String> l6 = load(file6);
+		List<String> l7 = load(file7);
+		List<String> l8 = load(file8);
+		
+		//List of MAC addresses found in multiple files
+		Map<String, Set<String>> results = new HashMap<>();
+		
+		//Check each MAC address in each file
+		for(String candidate : l1) {
+			compare(l2, candidate, file1, file2, results);
+			compare(l3, candidate, file1, file3, results);
+			compare(l4, candidate, file1, file4, results);
+			compare(l5, candidate, file1, file5, results);
+			compare(l6, candidate, file1, file6, results);
+			compare(l7, candidate, file1, file7, results);
+			compare(l8, candidate, file1, file8, results);
 		}
 		
-		List<String> l1 = load(args[0]);
-		List<String> l2 = load(args[1]);
-		List<String> results = new ArrayList<>();
+		for(String candidate : l2) {
+			compare(l3, candidate, file2, file3, results);
+			compare(l4, candidate, file2, file4, results);
+			compare(l5, candidate, file2, file5, results);
+			compare(l6, candidate, file2, file6, results);
+			compare(l7, candidate, file2, file7, results);
+			compare(l8, candidate, file2, file8, results);
+		}
 		
+		for(String candidate : l3) {
+			compare(l4, candidate, file3, file4, results);
+			compare(l5, candidate, file3, file5, results);
+			compare(l6, candidate, file3, file6, results);
+			compare(l7, candidate, file3, file7, results);
+			compare(l8, candidate, file3, file8, results);
+		}
+		
+		for(String candidate : l4) {
+			compare(l5, candidate, file4, file5, results);
+			compare(l6, candidate, file4, file6, results);
+			compare(l7, candidate, file4, file7, results);
+			compare(l8, candidate, file4, file8, results);
+		}
+		
+		for(String candidate : l5) {
+			compare(l6, candidate, file5, file6, results);
+			compare(l7, candidate, file5, file7, results);
+			compare(l8, candidate, file5, file8, results);
+		}
+		
+		for(String candidate : l6) {
+			compare(l7, candidate, file6, file7, results);
+			compare(l8, candidate, file6, file8, results);
+		}
+		
+		for(String candidate : l7) {
+			compare(l8, candidate, file7, file8, results);
+		}
+		/*
 		for(String candidate : l1) {
 			if(l2.contains(candidate)) {
-				results.add(candidate);
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file1);
+					results.get(candidate).add(file2);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file1);
+					results.get(candidate).add(file2);
+				}
+			}
+			
+			if(l3.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file1);
+					results.get(candidate).add(file3);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file1);
+					results.get(candidate).add(file3);
+				}
+			}
+			
+			if(l4.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file1);
+					results.get(candidate).add(file4);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file1);
+					results.get(candidate).add(file4);
+				}
+			}
+			
+			if(l5.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file1);
+					results.get(candidate).add(file5);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file1);
+					results.get(candidate).add(file5);
+				}
+			}
+			
+			if(l6.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file1);
+					results.get(candidate).add(file6);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file1);
+					results.get(candidate).add(file6);
+				}
+			}
+			
+			if(l7.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file1);
+					results.get(candidate).add(file7);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file1);
+					results.get(candidate).add(file7);
+				}
+			}
+			
+			if(l8.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file1);
+					results.get(candidate).add(file8);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file1);
+					results.get(candidate).add(file8);
+				}
+			}
+			
+		}
+		
+		for(String candidate : l2) {		
+			if(l3.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file2);
+					results.get(candidate).add(file3);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file2);
+					results.get(candidate).add(file3);
+				}
+			}
+			
+			if(l4.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file2);
+					results.get(candidate).add(file4);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file2);
+					results.get(candidate).add(file4);
+				}
+			}
+			
+			if(l5.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file2);
+					results.get(candidate).add(file5);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file2);
+					results.get(candidate).add(file5);
+				}
+			}
+			
+			if(l6.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file2);
+					results.get(candidate).add(file6);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file2);
+					results.get(candidate).add(file6);
+				}
+			}
+			
+			if(l7.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file2);
+					results.get(candidate).add(file7);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file2);
+					results.get(candidate).add(file7);
+				}
+			}
+			
+			if(l8.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file2);
+					results.get(candidate).add(file8);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file2);
+					results.get(candidate).add(file8);
+				}
+			}
+			
+		}
+		
+		for(String candidate : l3) {		
+			if(l4.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file3);
+					results.get(candidate).add(file4);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file3);
+					results.get(candidate).add(file4);
+				}
+			}
+			
+			if(l5.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file3);
+					results.get(candidate).add(file5);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file3);
+					results.get(candidate).add(file5);
+				}
+			}
+			
+			if(l6.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file3);
+					results.get(candidate).add(file6);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file3);
+					results.get(candidate).add(file6);
+				}
+			}
+			
+			if(l7.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file3);
+					results.get(candidate).add(file7);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file3);
+					results.get(candidate).add(file7);
+				}
+			}
+			
+			if(l8.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file3);
+					results.get(candidate).add(file8);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file3);
+					results.get(candidate).add(file8);
+				}
+			}
+			
+		}
+		
+		for(String candidate : l4) {					
+			if(l5.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file4);
+					results.get(candidate).add(file5);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file4);
+					results.get(candidate).add(file5);
+				}
+			}
+			
+			if(l6.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file4);
+					results.get(candidate).add(file6);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file4);
+					results.get(candidate).add(file6);
+				}
+			}
+			
+			if(l7.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file4);
+					results.get(candidate).add(file7);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file4);
+					results.get(candidate).add(file7);
+				}
+			}
+			
+			if(l8.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file4);
+					results.get(candidate).add(file8);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file4);
+					results.get(candidate).add(file8);
+				}
+			}
+			
+		}
+		
+		for(String candidate : l5) {							
+			if(l6.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file5);
+					results.get(candidate).add(file6);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file5);
+					results.get(candidate).add(file6);
+				}
+			}
+			
+			if(l7.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file5);
+					results.get(candidate).add(file7);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file5);
+					results.get(candidate).add(file7);
+				}
+			}
+			
+			if(l8.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file5);
+					results.get(candidate).add(file8);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file5);
+					results.get(candidate).add(file8);
+				}
+			}
+			
+		}
+		
+		for(String candidate : l6) {							
+			if(l7.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file6);
+					results.get(candidate).add(file7);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file6);
+					results.get(candidate).add(file7);
+				}
+			}
+			
+			if(l8.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file6);
+					results.get(candidate).add(file8);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file6);
+					results.get(candidate).add(file8);
+				}
 			}
 		}
 		
+		for(String candidate : l7) {							
+			if(l8.contains(candidate)) {
+				if(results.containsKey(candidate)) {
+					results.get(candidate).add(file7);
+					results.get(candidate).add(file8);
+				} else {
+					results.put(candidate, new HashSet<>());
+					results.get(candidate).add(file7);
+					results.get(candidate).add(file8);
+				}
+			}
+		}
+		*/
+		//Output results
 		if(results.isEmpty()) {
 			System.out.println("No common MAC addresses found.");
 		} else {
-			System.out.println("Common MAC addresses found:");
-			for(String a : results) {
-				System.out.println(a);
+			System.out.println( results.keySet().size() + " common MAC addresses found:");
+			for(String a : results.keySet()) {
+				if(results.get(a) == null) {
+					System.out.println(a);
+				} else {
+					
+					System.out.println(a);
+					for(String b : results.get(a)){
+					System.out.println("\t"+b);
+				}
 			}
 		}
-		
+		}
+	}
+	
+	public static void compare(List<String> list, String candidate, String file1, String file2, Map<String, Set<String>> results) {
+		if(list.contains(candidate)) {
+			if(results.containsKey(candidate)) {
+				results.get(candidate).add(file1);
+				results.get(candidate).add(file2);
+			} else {
+				results.put(candidate, new HashSet<>());
+				results.get(candidate).add(file1);
+				results.get(candidate).add(file2);
+			}
+		}
 	}
 	
 	@SuppressWarnings("resource")
