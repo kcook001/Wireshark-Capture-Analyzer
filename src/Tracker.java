c4a4bfcb08a5735ab8ea101a2ad22fb5788e3c69
kcook001 <kcook001@gmail.com>
2018-06-01 01:14:53 -0700

    Add vendor analysis

--- src/Tracker.java
+++ src/Tracker.java
@@ -1,8 +1,15 @@
+// CS498 Wireshark Capture Analysis
+// Keith Cook, Spring 2018
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -27,19 +34,26 @@ public class Tracker {
 		String file11 = "BStest2.csv";
 		String file12 = "BStest3.csv";
 		
+		//List of vendors from MAC addresses found in the capture
+		Map<String, Integer> vendorsSeen = new HashMap<>();
+		
+		//List of known vendor MACs, loaded from a file.
+		Map<String, String> vendors = vendorLoad("oui.txt");
+		
+		
 		//MAC addresses
-		List<String> l1 = load(file1);
-		List<String> l2 = load(file2);
-		List<String> l3 = load(file3);
-		List<String> l4 = load(file4);
-		List<String> l5 = load(file5);
-		List<String> l6 = load(file6);
-		List<String> l7 = load(file7);
-		List<String> l8 = load(file8);
-		List<String> l9 = load(file9);
-		List<String> l10 = load(file10);
-		List<String> l11 = load(file11);
-		List<String> l12 = load(file12);
+		List<String> l1 = load(file1, vendorsSeen, vendors);
+		List<String> l2 = load(file2, vendorsSeen, vendors);
+		List<String> l3 = load(file3, vendorsSeen, vendors);
+		List<String> l4 = load(file4, vendorsSeen, vendors);
+		List<String> l5 = load(file5, vendorsSeen, vendors);
+		List<String> l6 = load(file6, vendorsSeen, vendors);
+		List<String> l7 = load(file7, vendorsSeen, vendors);
+		List<String> l8 = load(file8, vendorsSeen, vendors);
+		List<String> l9 = load(file9, vendorsSeen, vendors);
+		List<String> l10 = load(file10, vendorsSeen, vendors);
+		List<String> l11 = load(file11, vendorsSeen, vendors);
+		List<String> l12 = load(file12, vendorsSeen, vendors);
 		
 		//List of MAC addresses found in multiple files
 		Map<String, Set<String>> results = new HashMap<>();
@@ -161,6 +175,26 @@ public class Tracker {
 			}
 		}
 		}
+		
+		
+		Map<String, Integer> sorted = sortByValue(vendorsSeen);
+		int users = 0;
+		
+		
+		System.out.println("\n\nVendor Frequency:");
+		for(String v : sorted.keySet()) {
+			System.out.println(v + " : " + sorted.get(v));
+			users += sorted.get(v);
+		}
+		System.out.println("\nDistinct users: " + users);
+		System.out.println("Distinct vendor numbers: " + sorted.keySet().size());
+		
+		/*
+		System.out.println("\n\n Known vendor list:");
+		for(String v : vendors.keySet()) {
+			System.out.println(v + " : " + vendors.get(v));
+		}
+		*/
 	}
 	
 	//Check whether a MAC address from one file is in another.  If it is, add it and the names of the files it was found in to the results list. 
@@ -179,21 +213,35 @@ public class Tracker {
 	
 	//Load MAC addresses from file to List data structure.
 	@SuppressWarnings("resource")
-	public static List<String> load(String filename) throws FileNotFoundException {
+	public static List<String> load(String filename, Map<String, Integer> vendorsSeen, Map<String, String> vendors) throws FileNotFoundException {
 		
 		List<String> list = new ArrayList<>();
 		
 		String line;
 		String mac;
+		String v;
+		String vname;
 		
 		Scanner sc = new Scanner(new File(filename)).useDelimiter("\\n");
 		
 		while(sc.hasNext()) {
-			//candidate = inFile1.next();
 			line = sc.nextLine();
 			if(line.length() > 17 && !line.startsWith("BSSID") && !line.startsWith("Station")) {
 				mac = line.substring(0, 17);
 				list.add(mac);
+				v = mac.substring(0, 8);
+				vname = vendors.get(v);
+				if(vname != null) {
+					if(vendorsSeen.containsKey(vname))
+						vendorsSeen.put(vname, vendorsSeen.get(vname) + 1);
+					else
+						vendorsSeen.put(vname, 1);
+				} else {
+					if(vendorsSeen.containsKey("Unknown"))
+						vendorsSeen.put("Unknown", vendorsSeen.get("Unknown") + 1);
+					else
+						vendorsSeen.put("Unknown", 1);
+				}
 			}
 		}
 		
@@ -201,4 +249,54 @@ public class Tracker {
 		return list;
 	}
 	
+	@SuppressWarnings("resource")
+	public static Map<String, String> vendorLoad(String filename) throws FileNotFoundException {
+		
+		Map<String, String> results = new HashMap<>();
+		String line;
+		String mac;
+		String vendor;
+		//int count = 0;
+		
+		Scanner sc = new Scanner(new File(filename), "UTF-8").useDelimiter("\\n");
+		
+		while(sc.hasNext()) {
+			line = sc.nextLine();
+			//if(count%6 == 1) {
+			if(line.matches("^[0-9A-F]{2}-.*")) {
+				mac = line.substring(0, 8);
+				mac = mac.replaceAll("-", ":");
+				vendor = line.substring(18);
+				if(!vendor.isEmpty())
+					results.put(mac, vendor);
+			}
+			//count++;
+		}
+		//System.out.println(count);
+		
+		sc.close();
+		return results;
+	}
+	
+	// Sort for Map<String, Integer>
+	// Not mine, found online.
+	private static Map<String, Integer> sortByValue(Map<String, Integer> unsortMap) {
+
+        List<Map.Entry<String, Integer>> list =
+                new LinkedList<Map.Entry<String, Integer>>(unsortMap.entrySet());
+
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2) {
+                return (o2.getValue()).compareTo(o1.getValue());
+            }
+        });
+
+        Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+        for (Map.Entry<String, Integer> entry : list) {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+        
+        return sortedMap;
+    }
 }
